6bef29187807a29802a33a0c201c81c99fdebb0e
John Geer <john@jgeer.com>
2022-11-18 14:58:27 -0500

    Add distribution by company

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM rocker/tidyverse:4.2.2
 
 RUN apt-get update && apt-get install -y fish ranger
 
-# RUN useradd -ms /bin/bash rocker
+RUN install2.r xgboost
+
 USER rstudio
 WORKDIR /home/rstudio
 
--- main.R
+++ main.R
@@ -1,3 +1,4 @@
+library(xgboost)
 library(tidyverse)
 
 clean_chocolate_data = function(choc_df) {
@@ -24,6 +25,31 @@ plot_rating_dist = function(choc_df) {
   ggsave("images/rating_dist_by_year.png", p, width=600, height=600, unit="px", dpi=100)
 }
 
+plot_rating_dist_company = function(choc_df) {
+  # Plot the distribution of ratings across years
+  company_df = choc_df %>%
+    group_by(company) %>%
+    summarize(n = n(), mean = mean(rating)) %>%
+    arrange(desc(n)) %>%
+    head(18) %>%
+    arrange(desc(mean))
+
+  plot_df = choc_df %>%
+    filter(company %in% company_df$company) %>%
+    mutate(company = factor(company, levels = company_df$company))
+
+  p = ggplot(data = plot_df, aes(x = rating)) +
+    geom_density(fill = "#7B3F00", adjust=2) +
+    facet_wrap(vars(company), scales="free_y", ncol=3) +
+    labs(title = "Distribution of Chocolate Ratings", subtitle = "By Company") +
+    theme_light() +
+    theme(
+      axis.text.y=element_blank(),
+      axis.ticks.y=element_blank())
+
+  ggsave("images/rating_dist_by_company.png", p, width=600, height=600, unit="px", dpi=100)
+}
+
 top_chocolates = function(choc_df, n) {
   # Get the most recent, highest rated ones
   choc_df %>%
@@ -31,13 +57,30 @@ top_chocolates = function(choc_df, n) {
     slice(1:n)
 }
 
+feature_importance = function(given_df) {
+  # Explore what seems to matter
+  options(na.action="na.pass")
+  model_matrix <- model.matrix(rating ~ ., data = given_df, na.action="na.pass")
+  model = xgboost(model_matrix, label = given_df$rating, objective = "reg:squarederror", nrounds=100, max.depth = 4, eta = 1, nthread = 2)
+
+  importance <- xgb.importance(feature_names = colnames(model_matrix), model = model)
+  head(importance, 100)
+}
+
 main = function() {
   choc_df = read_csv('data/flavors_of_cacao.csv') %>%
     clean_chocolate_data()
 
+  plot_rating_dist_company(choc_df)
+  stop("Stopping for now")
+
+  feature_importance(choc_df)
+
+  # Show the top 10 best rated chocolates (more recent years perfered)
   top_chocolates(choc_df, 10) %>%
     print()
 
+  # Plot the chocolate rating by years
   plot_rating_dist(choc_df)
 
 }
